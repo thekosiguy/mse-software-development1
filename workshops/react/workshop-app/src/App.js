ce37caf02ffca8eba300f53f4f19603f2f762100
dearshrewdwit <dearshrewdwit@gmail.com>
2021-04-12 12:41:07 +0100

    add react workshops

---
+++ workshops/react/workshop-app/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+import WorkshopHeading from './WorkshopHeading'
+
+function App() {
+  return (
+    <div className="App">
+
+      {/* Change the title props accordingly */}
+      <WorkshopHeading title="Stateful v Stateless Components"/>
+
+      {/* Render different components below, eg: */}
+      {/* ---------------- */}
+    </div>
+  )
+}
+
+export default App;
---
+++ workshops/react/workshop-app/src/WorkshopHeading.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const WorkshopHeading = (props) => <h1 className="workshop-heading">DFA Workshop: {props.title}</h1>
+
+export default WorkshopHeading
