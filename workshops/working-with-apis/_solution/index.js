738873f80c3340fa787ca0de97d431faede752fa
dearshrewdwit <dearshrewdwit@gmail.com>
2021-07-16 08:32:59 +0100

    hide key

--- workshops/working-with-apis/_solution/index.js
+++ workshops/working-with-apis/_solution/index.js
@@ -1,7 +1,7 @@
 // implement me
 
 window.addEventListener('load', function() {
-  const appKey = "6173e6646d88596bf16603decfee950c"
+  const appKey = "ADD YOUR KEY HERE"
   $.get(`https://api.openweathermap.org/data/2.5/weather?q=london&appid=${appKey}&units=metric`, function(data) {
     const appElement = document.getElementById("app")
     appElement.innerText = `The temperature is ${data.main.temp}C`
