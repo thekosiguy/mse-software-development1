a8088fffecfd65c3739efa6c6dd648c0ab6b88c8
Nkosilathi Sibanda <kosi53@hotmail.co.uk>
2021-07-16 17:33:11 +0100

    more updates

--- workshops/working-with-apis/cypress/integration/weather.spec.js
+++ workshops/working-with-apis/cypress/integration/weather.spec.js
@@ -1,3 +1,9 @@
 xdescribe('weather data', function() {
 // implement me
+    it('gets data from the weather api', function () {
+        const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=7a57d9f63f374d9200b00c0b9c8e78dd"
+        cy.intercept(apiUrl, {fixture: '../fixtures/weather.json'})
+        cy.visit('/').
+        cy.get('#name').should('contain', 'London')
+    })
 })
